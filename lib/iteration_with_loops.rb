78b565c8a9e42e4338d35be618be2c81daeeafd1
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-21 05:48:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,10 +6,10 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    if src[row_index][element_index] % 2 == 0
+    if src[row_index][element_index].even?
       p src[row_index][elemtent_index]
-    element_index += 1
-  else element_index += 1 
+    end 
+   element_index += 1 
   end
   row_index += 1
 end #=> nil
